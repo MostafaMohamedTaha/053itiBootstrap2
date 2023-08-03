184cc446334f512845c0c16477eb85d049c6b550
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-08-03 07:09:19 +0300

    markup

---
+++ src/notePages/main.js
@@ -0,0 +1,19 @@
+// (((((((((((((((((start notes)))))))))))))))))
+
+// container>row.row-cols-5>col*5 
+// container>row.align-items-center[start|end]>col*5 
+//container>row.justify-content-center[start|end|around|between]>col*5
+// container>row.offset-3>col-3*3
+// container>row>col-6>row>col-3*3
+//mx,my,mx-md-2,p-2,mx,py,pt,pb,pr
+//border border-2 border-info rounded-3 rounded-pill
+//img-thumbnail img-fluid 
+//d-none d-block 
+//shadow-1
+//typography h1=>6 display1=>6
+//text-capitalize
+//.table make table table-hover table-strapped table-dark table-responsive
+//table thead,th,td tbody 
+//button btn btn-primary btn-lg btn-outline-primary
+//form container>row<form>.form-label | form-control | form-check | form-check-input | form-check-label | form-row form-floating</form>
+
